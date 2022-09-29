dd8971b160b2eabb9a85849d3da796f2e1486567
HuiHuiKoh <66913677+HuiHuiKoh@users.noreply.github.com>
2022-09-29 16:12:00 +0800

    profile

--- app/src/main/java/com/example/monee/profile/ProfileFragment.kt
+++ app/src/main/java/com/example/monee/profile/ProfileFragment.kt
@@ -7,18 +7,18 @@ import android.view.View
 import android.view.ViewGroup
 import com.example.monee.R
 
+/**
 // TODO: Rename parameter arguments, choose names that match
 // the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
 private const val ARG_PARAM1 = "param1"
 private const val ARG_PARAM2 = "param2"
-
-/**
  * A simple [Fragment] subclass.
  * Use the [ProfileFragment.newInstance] factory method to
  * create an instance of this fragment.
  */
+
 class ProfileFragment : Fragment() {
-    // TODO: Rename and change types of parameters
+    /*// TODO: Rename and change types of parameters
     private var param1: String? = null
     private var param2: String? = null
 
@@ -28,7 +28,9 @@ class ProfileFragment : Fragment() {
             param1 = it.getString(ARG_PARAM1)
             param2 = it.getString(ARG_PARAM2)
         }
-    }
+    }*/
+
+
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
@@ -38,7 +40,7 @@ class ProfileFragment : Fragment() {
         return inflater.inflate(R.layout.fragment_profile, container, false)
     }
 
-    companion object {
+    /*companion object {
         /**
          * Use this factory method to create a new instance of
          * this fragment using the provided parameters.
@@ -56,5 +58,5 @@ class ProfileFragment : Fragment() {
                     putString(ARG_PARAM2, param2)
                 }
             }
-    }
+    }*/
 }
