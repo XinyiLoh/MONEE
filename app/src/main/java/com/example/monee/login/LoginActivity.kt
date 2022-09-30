0404761396d78fe11816d59e95359279794fe71d
HuiHuiKoh <66913677+HuiHuiKoh@users.noreply.github.com>
2022-09-30 10:10:34 +0800

    login

--- app/src/main/java/com/example/monee/login/LoginActivity.kt
+++ app/src/main/java/com/example/monee/login/LoginActivity.kt
@@ -1,29 +1,36 @@
-package com.example.monee
+package com.example.monee.login
 
 import android.content.Intent
 import android.os.Bundle
 import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
+import com.example.monee.MainActivity
+import com.example.monee.R
 import com.example.monee.databinding.ActivityLoginBinding
+import com.google.firebase.auth.FirebaseAuth
 
 class LoginActivity : AppCompatActivity() {
 
     private lateinit var binding: ActivityLoginBinding
-    private val name = binding.editName.text
-    private val email = binding.editEmail.text
-    private val phone = binding.editPhone.text
-    private val pw = binding.editPw.text
-    private val btnLogin = binding.btnLogin
-    private val toRegister = binding.textRegister
-    //private var counter: Int = 0
+    private lateinit var auth: FirebaseAuth
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         binding = ActivityLoginBinding.inflate(layoutInflater)
         setContentView(R.layout.activity_login)
 
+        auth = FirebaseAuth.getInstance()
+
+        val name = binding.editName.text.toString()
+        val email = binding.editEmail.text.toString()
+        val phone = binding.editPhone.text.toString()
+        val pw = binding.editPw.text.toString()
+        val btnLogin = binding.btnLogin
+        val toRegister = binding.textRegister
+
         btnLogin.setOnClickListener {
-            validate(name.toString(), phone.toString(), email.toString(), pw.toString())
+            validate(name, phone, email, pw)
+            loginUser(name, phone, email, pw)
         }
 
         toRegister.setOnClickListener {
@@ -46,14 +53,24 @@ class LoginActivity : AppCompatActivity() {
         ) {
             Toast.makeText(this, "Please fill in all fields", Toast.LENGTH_SHORT).show()
         }
-        /*else {
-            counter--
+    }
 
-            info.setText("No. of attempts remaining: " + counter)
+    private fun loginUser(
+        userName: String,
+        userPhone: String,
+        userEmail: String,
+        userPassword: String
+    ) {
+        auth.signInWithEmailAndPassword(userEmail, userPassword)
+            .addOnSuccessListener {
+                Toast.makeText(baseContext, "Login Successfull", Toast.LENGTH_LONG).show();
 
-            if (counter == 0) {
-                btnLogin.isEnabled = false
+                //to home page home fragment
+                startActivity(Intent(this, MainActivity::class.java))
             }
-        }*/
+            .addOnFailureListener { exception ->
+                Toast.makeText(baseContext, exception.message.toString(), Toast.LENGTH_LONG).show();
+            }
+
     }
 }
