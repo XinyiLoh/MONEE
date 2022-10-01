cbda51bc6e726f5b25fab64eade497170067e77d
XinyiLoh <lohxy-wm19@student.tarc.edu.my>
2022-10-01 18:42:37 +0800

    calculator

--- app/src/main/java/com/example/monee/calculator/CarLoanFragment.kt
+++ app/src/main/java/com/example/monee/calculator/CarLoanFragment.kt
@@ -6,8 +6,10 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.Toast
 import com.example.monee.R
 import com.example.monee.databinding.FragmentCarLoanBinding
+import kotlinx.android.synthetic.main.fragment_car_loan.*
 
 
 class CarLoanFragment : Fragment() {
@@ -31,13 +33,47 @@ class CarLoanFragment : Fragment() {
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
-        //val manager: PackageManager = context?.packageManager!!
-        //val appInfo = manager.getPackageInfo(context?.packageName!!, PackageManager.GET_ACTIVITIES)
+
+        binding.btnCalculateCarLoan.setOnClickListener{
+            calculateCarLoan()
+        }
 
         binding.carLoanResource.setOnClickListener {
             val intent = Intent(context, IMoneyCarLoanActivity::class.java)
             startActivity(intent)
         }
+    }
+
+    private fun isNumeric(toCheck: String): Boolean {
+        val regex = "-?[0-9]+(\\.[0-9]+)?".toRegex()
+        return toCheck.matches(regex)
+    }
+
+    private fun calculateCarLoan() {
+
+        val carPrice = _binding?.vehiclePrice.toString()
+        val downPayment = _binding?.downPayment.toString()
+        val yrs = _binding?.loanPeriod.toString()
+        val rate = _binding?.interestRate.toString()
+
+        vehicle_price.helperText = isNumeric(carPrice).toString()
+        down_payment.helperText = isNumeric(downPayment).toString()
+        loan_period.helperText = isNumeric(yrs).toString()
+        interest_rate.helperText = isNumeric(rate).toString()
+
+        val checkCarPrice = vehicle_price.helperText == null
+        val checkDownPayment = down_payment.helperText == null
+        val checkLoanPeriod = loan_period.helperText == null
+        val checkInterest = interest_rate.helperText == null
+
+        if (checkCarPrice && checkDownPayment && checkLoanPeriod && checkInterest) {
+            //var monthlyPayment = carPrice
+        }
+    }
+
+    override fun onDestroyView() {
+        super.onDestroyView()
+        _binding = null
     }
 
 }
