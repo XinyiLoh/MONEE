b1f89162929d3211bcd8bf22d194b4d1b0f2d6ad
XinyiLoh <lohxy-wm19@student.tarc.edu.my>
2022-10-02 04:28:48 +0800

    daily

---
+++ app/src/main/java/com/example/monee/report/DailyAdapter.kt
@@ -0,0 +1,38 @@
+package com.example.monee.report
+
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import android.widget.TextView
+import androidx.recyclerview.widget.RecyclerView
+import com.example.monee.R
+import com.example.monee.home.data.Categories
+
+class DailyAdapter(private val dailyList: ArrayList<Categories>) :
+    RecyclerView.Adapter<DailyAdapter.ViewHolder>() {
+
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): ViewHolder {
+        val itemView = LayoutInflater.from(parent.context).inflate(R.layout.fragment_dailylist_custom, parent, false)
+        return ViewHolder(itemView)
+    }
+
+    override fun onBindViewHolder(holder: ViewHolder, position: Int) {
+        val currentDaily = dailyList[position]
+        holder.tvCat.text = currentDaily.category
+        holder.tvFlow.text = currentDaily.type
+        holder.tvAmount.text = currentDaily.amount.toString()
+    }
+
+    override fun getItemCount(): Int {
+        return dailyList.size
+    }
+
+    class ViewHolder(itemView: View) : RecyclerView.ViewHolder(itemView) {
+
+        val tvFlow : TextView = itemView.findViewById(R.id.textView_flow)
+        val tvCat : TextView = itemView.findViewById(R.id.textView_cat)
+        val tvAmount : TextView = itemView.findViewById(R.id.textViewAmount)
+
+    }
+
+}
--- app/src/main/java/com/example/monee/report/DailyFragment.kt
+++ app/src/main/java/com/example/monee/report/DailyFragment.kt
@@ -5,17 +5,25 @@ import android.util.Log
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.TextView
+import android.widget.Toast
 import androidx.fragment.app.Fragment
+import androidx.recyclerview.widget.RecyclerView
 import com.example.monee.R
 import com.example.monee.databinding.FragmentDailyBinding
 import com.example.monee.home.data.Categories
 import com.google.firebase.firestore.FirebaseFirestore
 import kotlinx.android.synthetic.main.fragment_daily.*
+import com.google.firebase.firestore.*
 
 class DailyFragment : Fragment() {
 
     private var _binding : FragmentDailyBinding? = null
     private val binding get() = _binding!!
+
+    private lateinit var dailyRecyclerView: RecyclerView
+    private lateinit var tvLoadingData: TextView
+    private lateinit var dailyList: ArrayList<Categories>
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
@@ -36,8 +44,46 @@ class DailyFragment : Fragment() {
             val month = java.lang.String.valueOf(month)
 
             Log.d("date", curDate + year + month)
+
+            val dateSelected = curDate + "/" + month + "/" + year
+            //getDailyRecord(dateSelected)
+
         }
     }
+
+    //https://www.youtube.com/watch?v=DW-d0kalMvU
+    //
+    private fun getDailyRecord(string: String){
+        dailyRecyclerView.visibility = View.GONE
+        tvLoadingData.visibility = View.VISIBLE
+
+        //val firestore = FirebaseFirestore.getInstance()
+
+        //dbRef = FirebaseDatabase.getInstance().getReference("Employees")
+
+        //dbRef.addValueEventListener(object : ValueEventListener{override fun onDataChange(snapshot: DataSnapshot) {
+        //        dailyList.clear()
+        //        if (snapshot.exists()){
+        //            for (empSnap in snapshot.children){
+        //                val dailyData = empSnap.getValue(Categories::class.java)
+        //                dailyList.add(dailyData!!)
+        //            }
+         //           val mAdapter = DailyAdapter(dailyList)
+          //          dailyRecyclerView.adapter = mAdapter
+
+//                    dailyRecyclerView.visibility = View.VISIBLE
+  //                  tvLoadingData.visibility = View.GONE
+    //            }
+      //      }
+
+        //    override fun onCancelled(error: DatabaseError) {
+          //      Log.w("Error", databaseError.toString())
+            //}
+
+        //})
+
+    }
+
 
     private fun setUpFireStore(){
         val firestore = FirebaseFirestore.getInstance()
