f93b88983023b073bb5b14f3ef26721e86e67c65
XinyiLoh <lohxy-wm19@student.tarc.edu.my>
2022-10-02 16:24:58 +0800

    report

--- app/src/main/java/com/example/monee/report/SummaryFragment.kt
+++ app/src/main/java/com/example/monee/report/SummaryFragment.kt
@@ -1,33 +1,37 @@
 package com.example.monee.report
 
+import android.content.ContentValues.TAG
 import android.os.Bundle
+import android.util.Log
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.Toast
 import com.example.monee.R
+import com.example.monee.databinding.FragmentHomeBinding
+import com.example.monee.databinding.FragmentSummaryBinding
+import com.example.monee.home.data.Categories
+import com.google.firebase.database.DataSnapshot
+import com.google.firebase.database.DatabaseError
+import com.google.firebase.database.ValueEventListener
+import com.google.firebase.firestore.ktx.firestore
+import com.google.firebase.firestore.ktx.toObjects
+import com.google.firebase.ktx.Firebase
+import java.util.Objects
 
-// TODO: Rename parameter arguments, choose names that match
-// the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
-private const val ARG_PARAM1 = "param1"
-private const val ARG_PARAM2 = "param2"
+data class Category2(
+    val amount: Double = 0.00,
+    val type: String = ""
+)
 
-/**
- * A simple [Fragment] subclass.
- * Use the [SummaryFragment.newInstance] factory method to
- * create an instance of this fragment.
- */
 class SummaryFragment : Fragment() {
-    // TODO: Rename and change types of parameters
-    private var param1: String? = null
-    private var param2: String? = null
+
+    private var _binding: FragmentSummaryBinding?= null
+    private val binding get() = _binding!!
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        arguments?.let {
-            param1 = it.getString(ARG_PARAM1)
-            param2 = it.getString(ARG_PARAM2)
-        }
     }
 
     override fun onCreateView(
@@ -35,26 +39,57 @@ class SummaryFragment : Fragment() {
         savedInstanceState: Bundle?
     ): View? {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_summary, container, false)
+        //return inflater.inflate(R.layout.fragment_summary, container, false)
+
+        _binding = FragmentSummaryBinding.inflate(inflater, container, false)
+        return binding.root
     }
 
-    companion object {
-        /**
-         * Use this factory method to create a new instance of
-         * this fragment using the provided parameters.
-         *
-         * @param param1 Parameter 1.
-         * @param param2 Parameter 2.
-         * @return A new instance of fragment SummaryFragment.
-         */
-        // TODO: Rename and change types and number of parameters
-        @JvmStatic
-        fun newInstance(param1: String, param2: String) =
-            SummaryFragment().apply {
-                arguments = Bundle().apply {
-                    putString(ARG_PARAM1, param1)
-                    putString(ARG_PARAM2, param2)
-                }
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+
+        readIncomeData()
+        readExpensesData()
+
+        binding.fabPrint.setOnClickListener{
+
+        }
+    }
+
+    private fun readIncomeData(){
+
+        Firebase.firestore
+            .collection("categories")
+            .whereEqualTo("type","Income")
+            .get()
+            .addOnSuccessListener { snap->
+
+                val list = snap.toObjects<Category2>()
+                val totalIncome: Double = list.sumOf { it.amount }
+
+                binding.textViewTotalIncome.text = "RM" + totalIncome.toString()
+
+            }.addOnFailureListener {
+                Toast.makeText(context,"Failed to get data.",Toast.LENGTH_SHORT).show()
             }
     }
+
+    private fun readExpensesData(){
+
+        Firebase.firestore
+            .collection("categories")
+            .whereEqualTo("type","Outcome")
+            .get()
+            .addOnSuccessListener { snap->
+
+                val list = snap.toObjects<Category2>()
+                val totalOutcome: Double = list.sumOf { it.amount }
+
+                binding.textViewTotalExpenses.text = "RM" + totalOutcome.toString()
+
+            }.addOnFailureListener {
+                Toast.makeText(context,"Failed to get data.",Toast.LENGTH_SHORT).show()
+            }
+    }
+
 }
